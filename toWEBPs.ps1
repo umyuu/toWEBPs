ed1e8fbaabf4edd23af686bd65a1f59a5c043f22
umyuu <124dtiaka@gmail.com>
2024-06-03 07:42:17 +0900

    リファクタリング
    変数名の変更

--- toWEBPs.ps1
+++ toWEBPs.ps1
@@ -1,28 +1,29 @@
 Add-Type -AssemblyName System.Windows.Forms
-class Webp {
+class WebpConverter {
     # WebPの実行ファイルのダウンロードページ
-    [string]$DownloadPage
-    [string]$FilePath
+    [string]$DownloadPageUrl
+    # WebPの実行ファイル
+    [string]$ExecutablePath
     # 処理除外対象の拡張子の配列
-    [string[]]$ExcludedExtensions
+    [string[]]$ExcludedFileExtensions
     #[string]$Arguments
 
-    Webp(){
-        $this.DownloadPage = [string]"https://developers.google.com/speed/webp/download?hl=ja"
-        $this.FilePath = $PSScriptRoot + "\third_party\cwebp.exe"
-        $this.ExcludedExtensions = @(".webp", ".exe", ".bat", ".ps1", ".zip")
+    WebpConverter(){
+        $this.DownloadPageUrl = [string]"https://developers.google.com/speed/webp/download?hl=ja"
+        $this.ExecutablePath = [string]$PSScriptRoot + "\third_party\cwebp.exe"
+        $this.ExcludedFileExtensions = @(".webp", ".exe", ".bat", ".ps1", ".zip")
 	#$this.Arguments = ""
     }
     [bool]IsExecutableExists(){
-        return Test-Path -Path $this.FilePath
+        return Test-Path -Path $this.ExecutablePath
     }
     [string]ToString(){
-        return $this.FilePath
+        return $this.ExecutablePath
     }
 }
 
-function Convert-Webp {
-    param ([Webp]$webp, [string]$FileName)
+function Convert-ToWebp {
+    param ([WebpConverter]$webp, [string]$FileName)
     Write-Output "#Convert-Webp Start"
     if ($FileName.length -eq 0) {
         Write-Output "Skipping empty filename"
@@ -32,7 +33,7 @@ function Convert-Webp {
     Write-Output "source: $FileName"
 
     $Extension = [System.IO.Path]::GetExtension($FileName)
-    if ($webp.ExcludedExtensions -contains $Extension) {
+    if ($webp.ExcludedFileExtensions -contains $Extension) {
         Write-Output "Skipping file with extension $($Extension):  $($FileName)"
         return;
     }
@@ -42,7 +43,7 @@ function Convert-Webp {
     $Arguments = @("-preset photo", "-metadata icc", "-sharp_yuv", "-o" + $GenerateFileName, "-progress", "-short", "" +$FileName) -join " "
     Write-Output $webp.ToString()
     Write-Output $Arguments
-    Start-Process -FilePath $webp.FilePath -ArgumentList $Arguments
+    Start-Process -FilePath $webp.ExecutablePath -ArgumentList $Arguments
     #$webp.Convert($FileName)
     Write-Output "#Convert-Webp End"
 }
@@ -51,24 +52,24 @@ function Convert-Webp {
 $args = @($args)
 Write-Output "#Display Run Params $($PSScriptRoot) $($args)"
 
-[Webp]$webp = [Webp]::new()
+[WebpConverter]$webp = [WebpConverter]::new()
 Write-Output "#WebP Check configuration"
 
 if (-not $webp.IsExecutableExists()) {
-    $result = [System.Windows.Forms.MessageBox]::Show("`WebPの実行ファイルが存在しません。`n$($webp.FilePath)`n`nダウンロードページを開きますか？`n$($webp.DownloadPage)`nはい:開く、いいえ:アプリを終了する。", "確認:toWEBPs", [System.Windows.Forms.MessageBoxButtons]::YesNo, [System.Windows.Forms.MessageBoxIcon]::Question)
+    $result = [System.Windows.Forms.MessageBox]::Show("`WebPの実行ファイルが存在しません。`n$($webp.ExecutablePath)`n`nダウンロードページを開きますか？`n$($webp.DownloadPageUrl)`nはい:開く、いいえ:アプリを終了する。", "確認:toWEBPs", [System.Windows.Forms.MessageBoxButtons]::YesNo, [System.Windows.Forms.MessageBoxIcon]::Question)
     if ($result -eq "Yes") {
-        Start-Process $webp.DownloadPage
+        Start-Process $webp.DownloadPageUrl
         exit
     } else {
         Write-Output "Script execution cancelled by user."
         exit
     }
 }
-Write-Output "Exists OK $($webp.FilePath)"
+Write-Output "Exists OK $($webp.ExecutablePath)"
 
 Write-Output "#Main"
 foreach ($v in $args) {
-  Convert-Webp -webp $webp -FileName $v
+  Convert-ToWebp -webp $webp -FileName $v
 }
 Write-Output "#Main End"
 
